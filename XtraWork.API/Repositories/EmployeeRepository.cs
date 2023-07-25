5a67f8c461090db8133b407c37316f9ad845ef07
viniciusou <viniciusou@gmail.com>
2023-07-25 11:36:16 -0300

    Added employee repository

---
+++ XtraWork.API/Repositories/EmployeeRepository.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using XtraWork.API.Entities;
+
+namespace XtraWork.API.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly XtraWorkContext _context;
+
+        public EmployeeRepository(XtraWorkContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAll()
+        {
+            return await _context.Employees
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.LastName)
+                .ToListAsync();
+        }
+
+        public async Task<Employee> Get(Guid id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task<List<Employee>> Search(string keyword)
+        {
+            return await _context.Employees
+                .Where(x => x.FirstName.Contains(keyword) || x.LastName.Contains(keyword))
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.LastName)
+                .ToListAsync();
+        }
+
+        public async Task<Employee> Create(Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            _context.Add(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task<Employee> Update(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
