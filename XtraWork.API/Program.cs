9a71a4ada826a42b8d3d3593b4f63af8c0b2c8e1
viniciusou <viniciusou@gmail.com>
2023-07-26 09:06:49 -0300

    Added configurations

--- XtraWork.API/Program.cs
+++ XtraWork.API/Program.cs
@@ -1,6 +1,21 @@
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using XtraWork.API.Repositories;
+using XtraWork.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("XtraWork");
+builder.Services.AddDbContext<XtraWorkContext>(options => options.UseSqlServer(connectionString));
+
 // Add services to the container.
+builder.Services.AddScoped<TitleRepository>();
+builder.Services.AddScoped<EmployeeRepository>();
+
+builder.Services.AddScoped<TitleService>();
+builder.Services.AddScoped<EmployeeService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +23,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<XtraWorkContext>();
+context.Database.EnsureCreated();
+
+app.UseExceptionHandler(options => 
+{
+    options.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception != null)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = new 
+            {
+                message = exception.GetBaseException().Message
+            };
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    });
+    
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
