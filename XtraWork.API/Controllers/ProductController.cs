207668fa08e55c02096aa5f686acdd236bbd4158
viniciusou <viniciusou@gmail.com>
2023-07-26 15:26:49 -0300

    Added product controller

---
+++ XtraWork.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using XtraWork.API.Services;
+
+namespace XtraWork.API.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll(CancellationToken cancellationToken)
+        {
+            var response = await _productService.GetAll(cancellationToken);
+            return Ok(response);
+        }
+    }
+}
