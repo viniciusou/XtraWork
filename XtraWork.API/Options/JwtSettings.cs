acc0227dfd596b3d0b76a38125e0e9517db055ce
viniciusou <viniciusou@gmail.com>
2023-08-15 09:13:59 -0300

    Added jwt authentication

---
+++ XtraWork.API/Options/JwtSettings.cs
@@ -0,0 +1,7 @@
+namespace XtraWork.API.Options
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; }       
+    }
+}
--- XtraWork.API/Program.cs
+++ XtraWork.API/Program.cs
@@ -1,7 +1,13 @@
 using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using XtraWork.API.Extensions;
+using XtraWork.API.Options;
 using XtraWork.API.Repositories;
 using XtraWork.API.Services;
 
@@ -9,6 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("XtraWork");
 builder.Services.AddDbContext<XtraWorkContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<XtraWorkContext>();
 
 // Add services to the container.
 builder.Services.AddScoped<ITitleRepository, TitleRepository>();
@@ -20,13 +27,65 @@ builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.Decorate<IProductService, CachedProductService>();
 
+builder.Services.AddScoped<IIdentityService, IdentityService>();
+
+var jwtSettings = new JwtSettings();
+builder.Configuration.Bind(nameof(jwtSettings), jwtSettings);
+builder.Services.AddSingleton(jwtSettings);
+
 
 builder.Services.AddMemoryCache();
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(x => 
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x => 
+{
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = false,
+        ValidateLifetime = true 
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x => {
+    var security = new Dictionary<string, IEnumerable<string>>
+    {
+        {"Bearer", new string[0]}
+    };
+
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme 
+            {
+                Reference = new OpenApiReference 
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            }, new List<string>()
+        }   
+    });
+});
 
 var app = builder.Build();
 
@@ -61,6 +120,7 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
